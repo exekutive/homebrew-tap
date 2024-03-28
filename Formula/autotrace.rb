37ecef7fcfc5a9a7b256093ed53cd332d4bd4d58
Joe <exekutive@hotmail.com>
2024-03-27 21:56:42 -0700

    Update autotrace.rb

--- Formula/autotrace.rb
+++ Formula/autotrace.rb
@@ -1,5 +1,5 @@
 class Autotrace < Formula
-  desc "A program for converting bitmap to vector graphics."
+  desc "Program for converting bitmap to vector graphics"
   homepage "https://autotrace.sourceforge.net"
   url "https://downloads.sourceforge.net/project/autotrace/AutoTrace/0.31.1/autotrace-0.31.1.tar.gz"
   sha256 "5a1a923c3335dfd7cbcccb2bbd4cc3d68cafe7713686a2f46a1591ed8a92aff6"
@@ -11,15 +11,17 @@ class Autotrace < Formula
   def patches
     { :p0 => [
       "https://trac.macports.org/export/100575/trunk/dports/graphics/autotrace/files/patch-libpng-1.5.diff",
-      "https://trac.macports.org/export/77101/trunk/dports/graphics/autotrace/files/patch-autotrace.m4.diff"
+      "https://trac.macports.org/export/77101/trunk/dports/graphics/autotrace/files/patch-autotrace.m4.diff",
     ] }
   end
 
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}",
-            "--mandir=#{man}",]
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}",
+      "--mandir=#{man}",
+    ]
 
     args << "--without-magick" if build.without? 'imagemagick'
 
